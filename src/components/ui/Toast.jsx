e03a847094b5d8c75711515396913e8a9cbdbca5
hyejin <yhj970102@gmail.com>
2023-07-11 16:45:37 +0900

    Toast 컴포넌트 구현

---
+++ src/components/ui/Toast.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+function Toast({ children }) {
+    const toast = useRef();
+
+    useEffect(() => {
+        toast.current.classList.add('Toast--show');
+
+        setTimeout(() => {
+            toast.current.classList.add('Toast--hidden');
+        }, 3000)
+    })
+
+    return (
+        <div className='Toast' ref={toast}>
+            {children}
+        </div>
+    );
+}
+
+export default Toast;
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,3 +3,4 @@ export { default as  Input } from './Input';
 export { default as  ImageFileInput } from './ImageFileInput';
 export { default as  Button } from './Button';
 export { default as  Modal } from './Modal';
+export { default as  Toast } from './Toast';
