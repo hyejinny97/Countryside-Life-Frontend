e7975888470230192fb548759724ac7b501cfe2d
hyejin <yhj970102@gmail.com>
2023-09-09 15:40:13 +0900

    Like Fetcher Form 대신 Form으로 변경

--- src/components/community/Like.jsx
+++ src/components/community/Like.jsx
@@ -1,26 +1,24 @@
-import { useSelector } from 'react-redux';
-import { useFetcher } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Form } from "react-router-dom";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
-function Like({ likeUsers=[] }) {
-    const fetcher = useFetcher();
-    const authenticatedUserId = useSelector(state => state.user.id);
+function Like({ likeUsers = [] }) {
+  const authenticatedUserId = useSelector((state) => state.user.id);
 
-    let liked = likeUsers.some(likeUser => likeUser === authenticatedUserId);
-    if (fetcher.formData) {
-        liked = fetcher.formData.get('like') === 'true';
-    }
+  let liked = likeUsers.some((likeUser) => likeUser === authenticatedUserId);
 
-    return (
-        <fetcher.Form method='post' className="Like">
-            <button
-                name='like'
-                value={liked ? 'false': 'true'}
-            >
-                {liked ? <AiFillHeart className="Like__full-heart" /> : <AiOutlineHeart className="Like__empty-heart" />}
-            </button>
-        </fetcher.Form>
-    );
+  return (
+    <Form method="post" className="Like">
+      <button name="like" value={liked ? "false" : "true"}>
+        {liked ? (
+          <AiFillHeart className="Like__full-heart" />
+        ) : (
+          <AiOutlineHeart className="Like__empty-heart" />
+        )}
+      </button>
+    </Form>
+  );
 }
 
 export default Like;
