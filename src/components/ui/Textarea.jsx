fe090548de7f53ec4ac5d0a01c21a37b4e860bd0
hyejin <yhj970102@gmail.com>
2023-09-12 22:48:07 +0900

    Input, Textarea에 required prop 추가

--- src/components/ui/Textarea.jsx
+++ src/components/ui/Textarea.jsx
@@ -1,74 +1,85 @@
-import PropTypes from 'prop-types';
-import classname from 'classnames';
+import PropTypes from "prop-types";
+import classname from "classnames";
 
-function Textarea({ 
-    name, 
-    label, 
-    maxLength, 
-    rows, 
-    cols,
-    helpText, 
-    invalidTexts, 
-    placeholder, 
-    left, 
-    right, 
-    className, 
-    value,
-    defaultValue,
-    onChange,
-}) {    
-    const textareaClassName = classname('Textarea__input', {
-        'Textarea__input--red': invalidTexts && invalidTexts.length >= 1 && (!left || !right),
-        'Textarea__input--no-line': left || right,
-    })
+function Textarea({
+  name,
+  label,
+  maxLength,
+  rows,
+  cols,
+  helpText,
+  invalidTexts,
+  placeholder,
+  left,
+  right,
+  className,
+  value,
+  defaultValue,
+  onChange,
+  required,
+}) {
+  const textareaClassName = classname("Textarea__input", {
+    "Textarea__input--red":
+      invalidTexts && invalidTexts.length >= 1 && (!left || !right),
+    "Textarea__input--no-line": left || right,
+  });
 
-    const textareaContainerClassName = classname('Textarea__input-container', {
-        'Textarea__input-container--no-line': !left && !right,
-    })
+  const textareaContainerClassName = classname("Textarea__input-container", {
+    "Textarea__input-container--no-line": !left && !right,
+  });
 
-    const renderValidTexts = invalidTexts?.map(text => {
-        return <p key={text} className='Textarea__invalid-text' >{text}</p>
-    })
-
+  const renderValidTexts = invalidTexts?.map((text) => {
     return (
-    <div className={`Textarea ${className}`}>
-        {label && <label className='Textarea__label' htmlFor={label}>{label}</label>}
-        {helpText && <p className='Textarea__help-text'>{helpText}</p>}
-        <div className={textareaContainerClassName}>
-            <div className='Textarea__input-left'>{left}</div>
-            <textarea 
-                className={textareaClassName} 
-                id={label} 
-                name={name} 
-                placeholder={placeholder} 
-                rows={rows}
-                maxLength={maxLength}
-                onChange={(e) => onChange(e.target.value)}
-                defaultValue={value}
-            >   
-            </textarea>
-            <div className='Textarea__input-right'>{right}</div>
-        </div>
-        {renderValidTexts}
-    </div>
+      <p key={text} className="Textarea__invalid-text">
+        {text}
+      </p>
     );
+  });
+
+  return (
+    <div className={`Textarea ${className}`}>
+      {label && (
+        <label className="Textarea__label" htmlFor={label}>
+          {label}
+        </label>
+      )}
+      {helpText && <p className="Textarea__help-text">{helpText}</p>}
+      <div className={textareaContainerClassName}>
+        <div className="Textarea__input-left">{left}</div>
+        <textarea
+          className={textareaClassName}
+          id={label}
+          name={name}
+          placeholder={placeholder}
+          rows={rows}
+          maxLength={maxLength}
+          onChange={(e) => onChange(e.target.value)}
+          defaultValue={value}
+          required={required}
+        ></textarea>
+        <div className="Textarea__input-right">{right}</div>
+      </div>
+      {renderValidTexts}
+    </div>
+  );
 }
 
 Textarea.propTypes = {
-    name: PropTypes.string,
-    label: PropTypes.string,
-    maxLength: PropTypes.number,
-    rows: PropTypes.number,
-    cols: PropTypes.number,
-    helpText: PropTypes.string,
-    invalidTexts: PropTypes.array, 
-    placeholder: PropTypes.string,
-    left: PropTypes.element,
-    right: PropTypes.element,
-    className: PropTypes.string,
-    value: PropTypes.string,
-    defaultValue: PropTypes.string,
-    onChange: PropTypes.func,
-}
+  name: PropTypes.string,
+  label: PropTypes.string,
+  maxLength: PropTypes.number,
+  rows: PropTypes.number,
+  cols: PropTypes.number,
+  helpText: PropTypes.string,
+  invalidTexts: PropTypes.array,
+  placeholder: PropTypes.string,
+  left: PropTypes.element,
+  right: PropTypes.element,
+  className: PropTypes.string,
+  value: PropTypes.string,
+  defaultValue: PropTypes.string,
+  onChange: PropTypes.func,
+  required: PropTypes.bool,
+};
 
 export default Textarea;
